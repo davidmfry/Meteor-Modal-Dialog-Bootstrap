9dd641dedcd347cfae96ff12fe29556674b2fb92
David Fry <david.fry.tv@gmail.com>
2015-09-14 10:31:46 -0700

    Init Commit

---
+++ client/templates/animals/animals.js
@@ -0,0 +1,6 @@
+
+Template.animals.helpers({
+    animals: function() {
+        return Animals.find({}, { sort: {rank:1}});
+    }
+});
---
+++ server/fixtures.js
@@ -0,0 +1,17 @@
+if (Animals.find().count() === 0)
+{
+    Animals.insert({
+        rank: 3,
+        name: "Cat"
+    });
+
+    Animals.insert({
+        rank: 1,
+        name: "Dog"
+    });
+
+    Animals.insert({
+        rank: 2,
+        name: "Zebra"
+    });
+}
