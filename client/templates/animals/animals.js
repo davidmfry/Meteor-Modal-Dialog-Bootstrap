c7bf4557917ca76e9550f837c24cc4dedce5b59f
David Fry <david.fry.tv@gmail.com>
2015-09-14 10:47:13 -0700

    Modal works

--- client/templates/animals/animals.js
+++ client/templates/animals/animals.js
@@ -4,3 +4,31 @@ Template.animals.helpers({
         return Animals.find({}, { sort: {rank:1}});
     }
 });
+
+Template.animals.events({
+   'click #add': function(e) {
+       e.preventDefault();
+       $('#animalsModal').modal('show');
+   }
+});
+
+Template.animalsModalTemplate.events({
+   'click #save': function(e) {
+       e.preventDefault();
+
+       var animal = {
+           name: $('#animalName').val()
+       };
+
+       Meteor.call('addAnimal', animal, function(error, result) {
+          if(error)
+          {
+              alert(error);
+          }
+       });
+
+       $('#animalsModal').modal('hide');
+
+
+   }
+});
